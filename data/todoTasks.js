5ea6b6638eb2634a18aece579ceda9f5e0ead1e9
Sofie Blom <ssofieblom@hotmail.com>
2022-01-29 12:34:06 +0100

    added todays date and time

--- data/todoTasks.js
+++ data/todoTasks.js
@@ -1,36 +1,36 @@
+let date_ob = new Date();
+
 const todoTasks = [
   {
     id: 1,
-    created: "datum&tid",
+    created: new Date(),
     description: "Dricka för mycket kaffe",
     done: true,
   },
   {
     id: 2,
-    created: "datum&tid",
+    created: new Date(),
     description: "Ta en promenad i regnet",
     done: false,
   },
   {
     id: 3,
-    created: "datum&tid",
+    created: new Date(),
     description: "Kolla på någon dålig film",
     done: false,
   },
   {
     id: 4,
-    created: "datum&tid",
+    created: new Date(),
     description: "Laga punktering på cykeln",
     done: false,
   },
   {
     id: 5,
-    created: "datum&tid",
+    created: new Date(),
     description: "Äta en jättegod frukost",
     done: true,
   },
 ];
 
 module.exports = todoTasks;
-
-console.log(todoTasks);
